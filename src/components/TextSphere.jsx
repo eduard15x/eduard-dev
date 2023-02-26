9c0983013453240837a10f7b312c0fd8bca7af19
Precup Eduard <precupeduard99@gmail.com>
2023-02-27 01:07:34 +0200

    add valuemaybe it fix tagcloud

--- src/components/TextSphere.jsx
+++ src/components/TextSphere.jsx
@@ -2,6 +2,12 @@ import { useEffect } from "react";
 import TagCloud from "TagCloud";
 
 const TextSphere = () => {
+    const width = window.innerWidth > 1700 ? 350 
+                : window.innerWidth > 1450 ? 300 
+                : window.innerWidth > 1350 ? 250 
+                : window.innerWidth > 1250 ? 210 
+                : window.innerWidth > 600 ? 190 : 145
+
     useEffect(() => {
         return () => {
             const container = '.tagcloud';
@@ -9,7 +15,7 @@ const TextSphere = () => {
                            'Express.js', 'MongoDB', 'JSON', 'Github', 'Sourcetree', 'Bitbucket', 'Jira', 'Confluence', 'npm'];
 
             const options = {
-                radius: 300,
+                radius: width,
                 maxSpeed: 'normal',
                 initSpeed: 'normal',
                 keep: true
@@ -17,15 +23,14 @@ const TextSphere = () => {
 
             TagCloud(container, texts, options);
         }
-        // eslint-disable-next-line 
-    }, []);
+    }, [width]);
     
     return (
         <div className="absolute top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%] z-[2]
                         pt-[400px] md:pt-[320px]
                         xl:left-auto xl:translate-x-0 xl:pt-0 xl:right-[50px] 3xl:right-[110px]">
             <div className="text-sphere">
-                <span className="tagcloud"></span>
+                <span className="tagcloud"><span className="opacity-0">.</span></span>
             </div>
         </div>
     );
