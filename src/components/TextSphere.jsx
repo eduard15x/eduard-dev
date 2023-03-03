b67b7991f0d0ecfec9577d5698cf240a29d159ae
Precup Eduard <precupeduard99@gmail.com>
2023-03-03 19:35:16 +0200

    .

--- src/components/TextSphere.jsx
+++ src/components/TextSphere.jsx
@@ -3,29 +3,6 @@ import TagCloud from "@frank-mayer/react-tag-cloud";
 
 
 const TextSphere = () => {
-    const width = window.innerWidth > 1700 ? 350 
-                : window.innerWidth > 1450 ? 300 
-                : window.innerWidth > 1350 ? 250 
-                : window.innerWidth > 1250 ? 210 
-                : window.innerWidth > 600 ? 190 : 145
-
-    // useEffect(() => {
-    //     return () => {
-    //         const container = '.tagcloud';
-    //         const texts = ['JavaScript', 'HTML', 'CSS', 'Sass', 'Bootstrap', 'Tailwind CSS', 'jQuery', 'React', 'Node.js',
-    //                        'Express.js', 'MongoDB', 'JSON', 'Github', 'Sourcetree', 'Bitbucket', 'Jira', 'Confluence', 'npm'];
-
-    //         const options = {
-    //             radius: width,
-    //             maxSpeed: 'normal',
-    //             initSpeed: 'normal',
-    //             keep: true
-    //         };
-
-    //         TagCloud(container, texts, options);
-    //     }
-    // }, [width]);
-    
     return (
         <div>
             <TagCloud
